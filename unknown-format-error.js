6f4bc160b094b8373fe3e22ede98e63bf5c9220d
Julien Fontanet <julien.fontanet@isonoe.net>
2014-10-12 12:31:53 +0200

    Use make-error.

--- unknown-format-error.js
+++ unknown-format-error.js
@@ -2,16 +2,8 @@
 
 //====================================================================
 
-// Error cannot be derived like others.
-// https://github.com/petkaantonov/bluebird/blob/master/API.md#catchfunction-errorclassfunction-predicate-function-handler---promise
-function UnknownFormatError(message) {
-  this.message = message;
-  this.name = 'UnknownFormatError';
-  Error.captureStackTrace(this, UnknownFormatError);
-}
-UnknownFormatError.prototype = Object.create(Error.prototype);
-UnknownFormatError.prototype.constructor = UnknownFormatError;
+var makeError = require('make-error');
 
 //====================================================================
 
-exports = module.exports = UnknownFormatError;
+exports = module.exports = makeError('UnknownFormatError');
