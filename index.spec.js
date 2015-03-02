7575cfa3f403888f02ad34d55d3aa54066d39760
Julien Fontanet <julien.fontanet@isonoe.net>
2015-03-02 15:23:21 +0100

    Test.

---
+++ index.spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+//====================================================================
+
+var loadConfig = require('./').load;
+
+var mock = require('mock-fs');
+
+//====================================================================
+
+describe('appConf', function () {
+  before(function () {
+    mock({
+      // Vendor config
+      '../../config.json': mock.file({
+        content: '{ "vendor": true, "foo": "vendor" }',
+      }),
+
+      // System
+      '/etc/test-app-conf/config.json': mock.file({
+        content: '{ "system": true, "foo": "system" }',
+      }),
+
+      // Global (user configuration)
+      // TODO
+
+
+      // Local
+      '../.test-app-conf.json': mock.file({
+        content: '{ "local.1": true, "foo": "local.1" }',
+      }),
+      '.test-app-conf.json': mock.file({
+        content: '{ "local.0": true, "foo": "local.0" }',
+      }),
+    });
+  });
+
+  after(function () {
+    mock.restore();
+  });
+
+  it('#load()', function () {
+    return loadConfig('test-app-conf').then(function (config) {
+      config.must.eql({
+        'local.0': true,
+        'local.1': true,
+        system: true,
+        vendor: true,
+
+        foo: 'local.0',
+      });
+    });
+  });
+});
