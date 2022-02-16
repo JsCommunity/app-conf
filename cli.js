cd91f356d18ff56726a16bbb5ef108de49427faa
Julien Fontanet <julien.fontanet@isonoe.net>
2022-02-16 11:05:35 +0100

    chore(cli): use `util.inspect` `sorted` option

--- cli.js
+++ cli.js
@@ -5,22 +5,6 @@
 const { load } = require("./index.js");
 const { inspect } = require("util");
 
-function deepSortObjects(value) {
-  if (value === null || typeof value !== "object") {
-    return value;
-  }
-
-  if (Array.isArray(value)) {
-    return value.map(deepSortObjects);
-  }
-
-  const result = {};
-  for (const key of Object.keys(value).sort()) {
-    result[key] = deepSortObjects(value[key]);
-  }
-  return result;
-}
-
 async function main(args) {
   const { stdout } = process;
 
@@ -36,18 +20,16 @@ ${name} v${version}
 
   stdout.write(
     inspect(
-      deepSortObjects(
-        await load(appName, {
-          appDir,
+      await load(appName, {
+        appDir,
 
-          // ignore vendor config (not relevant for this CLI)
-          entries:
-            appDir === undefined ? ["system", "global", "local"] : undefined,
+        // ignore vendor config (not relevant for this CLI)
+        entries:
+          appDir === undefined ? ["system", "global", "local"] : undefined,
 
-          ignoreUnknownFormats: true,
-        })
-      ),
-      { depth: Infinity, colors: true }
+        ignoreUnknownFormats: true,
+      }),
+      { colors: true, depth: Infinity, sorted: true }
     )
   );
   stdout.write("\n");
