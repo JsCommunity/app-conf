JsCommunity/app-conf

commit d13c46c9de165ae06454f0a782b2022f31b42a7e

chore: remove Bluebird dep

entries.js
@@ -2,16 +2,18 @@
 
 // ===================================================================
 
-const Bluebird = require("bluebird");
+const promisify = require("promise-toolbox/promisify");
 
-const fs$readFile = Bluebird.promisify(require("fs").readFile);
+const fs$readFile = promisify(require("fs").readFile);
 const j = require("path").join;
-const realpath = Bluebird.promisify(require("fs").realpath);
+const realpath = promisify(require("fs").realpath);
 const resolvePath = require("path").resolve;
 
 const flatten = require("lodash/flatten");
-const glob = Bluebird.promisify(require("glob"));
+const glob = promisify(require("glob"));
 const xdgBasedir = require("xdg-basedir");
+
+const pMap = require("./_pMap");
 
 // ===================================================================
 
@@ -43,35 +45,23 @@ module.exports = [
   // Default vendor configuration.
   {
     name: "vendor",
-    read: function(opts) {
-      // It is assumed that app-conf is in the `node_modules`
-      // directory of the owner package.
-      return Bluebird.map(glob(j(opts.appDir, "config.*")), readFile);
-    },
+    read: opts => pMap(glob(j(opts.appDir, "config.*")), readFile),
   },
 
   // Configuration for the whole system.
   {
     name: "system",
-    read: function(opts) {
-      const name = opts.name;
-
-      return Bluebird.map(glob(j("/etc", name, "config.*")), readFile);
-    },
+    read: opts => pMap(glob(j("/etc", opts.appName, "config.*")), readFile),
   },
 
   // Configuration for the current user.
   {
     name: "global",
-    read: function(opts) {
+    read: opts => {
       const configDir = xdgBasedir.config;
-      if (!configDir) {
-        return Bluebird.resolve([]);
-      }
-
-      const name = opts.name;
-
-      return Bluebird.map(glob(j(configDir, name, "config.*")), readFile);
+      return configDir === undefined
+        ? []
+        : pMap(glob(j(configDir, opts.appName, "config.*")), readFile);
     },
   },
 
@@ -79,8 +69,8 @@ module.exports = [
   // hierarchy).
   {
     name: "local",
-    read: function(opts) {
-      const name = opts.name;
+    read(opts) {
+      const { appName } = opts;
 
       // Compute the list of paths from the current directory to the
       // root directory.
@@ -88,18 +78,19 @@ module.exports = [
       let dir, prev;
       dir = process.cwd();
       while (dir !== prev) {
-        paths.push(j(dir, "." + name + ".*"));
+        paths.push(j(dir, "." + appName + ".*"));
         prev = dir;
         dir = resolvePath(dir, "..");
       }
 
-      return Bluebird.map(paths.reverse(), function(path) {
-        return glob(path, {
-          silent: true,
-        }).catch(ignoreAccessErrors);
-      })
-        .then(flatten)
-        .map(readFile);
+      return pMap(
+        pMap(paths.reverse(), path =>
+          glob(path, {
+            silent: true,
+          }).catch(ignoreAccessErrors)
+        ).then(flatten),
+        readFile
+      );
     },
   },
 ];

_pMap.js
@@ -1,0 +1,6 @@
+module.exports = function pMap(array, iteratee) {
+  const { then } = array;
+  return typeof then === "function"
+    ? then.call(array, array => pMap(array, iteratee))
+    : Promise.all(array.map(iteratee));
+};

index.js
@@ -2,66 +2,42 @@
 
 // ===================================================================
 
-const Bluebird = require("bluebird");
+const promisify = require("promise-toolbox/promisify");
 
 const dirname = require("path").dirname;
-const getFileStats = Bluebird.promisify(require("fs").stat);
+const getFileStats = promisify(require("fs").stat);
 const resolvePath = require("path").resolve;
 
 const debug = require("debug")("app-conf");
 const flatten = require("lodash/flatten");
 const isObject = require("lodash/isObject");
 const isString = require("lodash/isString");
-const map = require("lodash/map");
 const merge = require("lodash/merge");
 
 const entries = require("./entries");
+const pMap = require("./_pMap");
 const UnknownFormatError = require("./unknown-format-error");
 const unserialize = require("./serializers").unserialize;
 
 // ===================================================================
 
-function isPath(path) {
-  return getFileStats(path)
-    .then(function() {
-      return true;
-    })
-    .catch(function() {
-      return false;
-    });
-}
+const isPath = path => getFileStats(path).then(() => true, () => false);
 
 const RELATIVE_PATH_RE = /^\.{1,2}[/\\]/;
-function resolveRelativePaths(value, base) {
-  let path;
-
+async function resolveRelativePaths(value, base) {
   if (isString(value) && RELATIVE_PATH_RE.test(value)) {
-    path = resolvePath(base, value);
-
-    return isPath(path).then(function(isPath) {
-      if (isPath) {
-        return path;
-      }
-      return value;
-    });
+    const path = resolvePath(base, value);
+    return (await isPath(path)) ? path : value;
   }
 
   if (isObject(value)) {
-    const promises = map(value, function(item, key) {
-      return resolveRelativePaths(item, base).then(function(item) {
-        value[key] = item;
-      });
+    await pMap(Object.keys(value), async key => {
+      value[key] = await resolveRelativePaths(value[key], base);
     });
-    return Bluebird.all(promises).return(value);
+    return value;
   }
 
-  return Bluebird.resolve(value);
-}
-
-function noop() {}
-
-function rethrow(error) {
-  throw error;
+  return value;
 }
 
 // ===================================================================
@@ -72,36 +48,34 @@ function rethrow(error) {
 // keep this for compatibility.
 const DEFAULT_APP_DIR = dirname(dirname(__dirname));
 
-function load(name, opts) {
-  const defaults = merge({}, opts && opts.defaults);
-  const unknownFormatHandler = opts && opts.ignoreUnknownFormats ? noop : rethrow;
+async function load(appName, opts) {
+  const ignoreUnknownFormats =
+    (opts != null && opts.ignoreUnknownFormats) || false;
 
-  return Bluebird.map(entries, function(entry) {
-    return entry.read({
-      name: name,
-      appDir: (opts && opts.appDir) || DEFAULT_APP_DIR,
-    });
-  })
-    .then(flatten)
-    .each(function(file) {
-      debug(file.path);
-      return file;
-    })
-    .map(function(file) {
-      return new Bluebird(function(resolve) {
-        resolve(unserialize(file));
+  const files = flatten(
+    await pMap(entries, entry =>
+      entry.read({
+        appDir: (opts && opts.appDir) || DEFAULT_APP_DIR,
+        appName,
       })
-        .then(function(value) {
-          return resolveRelativePaths(value, dirname(file.path));
-        })
-        .catch(UnknownFormatError, unknownFormatHandler);
-    })
-    .each(function(value) {
-      if (value) {
-        merge(defaults, value);
+    )
+  );
+  files.forEach(_ => debug(_.path));
+  const data = await pMap(files, file => {
+    try {
+      return resolveRelativePaths(unserialize(file), dirname(file.path));
+    } catch (error) {
+      if (!(ignoreUnknownFormats && error instanceof UnknownFormatError)) {
+        throw error;
       }
-    })
-    .return(defaults);
+    }
+  });
+  return data.reduce((acc, cfg) => {
+    if (cfg !== undefined) {
+      merge(acc, cfg);
+    }
+    return acc;
+  }, merge({}, opts && opts.defaults));
 }
 
 // ===================================================================

commit e83ff97e63bb64a383600081d50d89b240f41f21

fix: EACCESS error handling for local config
Introduced by d13c46c

entries.js
@@ -29,7 +29,7 @@ function readFile(path) {
 }
 
 function ignoreAccessErrors(error) {
-  if (error.cause.code !== "EACCES") {
+  if (error.code !== "EACCES") {
     throw error;
   }
 